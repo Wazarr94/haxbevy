796ca26116406a135eb892c0e53782881c286244
Wazarr <jeje_04@live.com>
2023-05-28 19:04:53 +0200

    move stadiums to assets folder

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crate::parser::stadium::StadiumRaw;
 mod parser;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    for stadium_file in fs::read_dir("stadiums")? {
+    for stadium_file in fs::read_dir("assets/stadiums")? {
         let stadium_str = fs::read_to_string(stadium_file?.path())?;
         let stadium_value = parse_to_serde_value(&stadium_str, &ParseOptions::default())?.unwrap();
         let stadium_raw: StadiumRaw = serde_json::from_value(stadium_value)?;
