9ee86b2fd5ff90df080461dc0b2702f9559bf7b1
Wazarr <jeje_04@live.com>
2023-06-17 11:45:31 +0200

    get working menu with states

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use bevy::asset::*;
 use bevy::prelude::*;
-use bevy::reflect::TypeUuid;
+use bevy_egui::EguiPlugin;
 use bevy_prototype_lyon::prelude::*;
 use menu::MenuPlugin;
-use parser::stadium::Stadium;
-use parser::stadium::StadiumRaw;
-
-use jsonc_parser::{parse_to_serde_value, ParseOptions};
+use renderer::RendererPlugin;
 
 mod menu;
 mod parser;
@@ -18,14 +14,13 @@ fn main() {
     console_error_panic_hook::set_once();
 
     App::new()
-        .add_plugins(DefaultPlugins)
         .add_state::<AppState>()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_plugin(ShapePlugin)
         .add_plugin(MenuPlugin)
-        // .add_plugin(WorldInspectorPlugin::new())
-        // .add_plugin(RendererPlugin)
-        .add_asset::<StadiumAsset>()
-        .init_asset_loader::<StadiumLoader>()
+        .add_plugin(RendererPlugin)
+        .add_startup_system(setup)
         .run();
 }
 
@@ -35,36 +30,13 @@ enum AppState {
     Menu,
     InGame,
 }
-#[derive(Debug, TypeUuid)]
-#[uuid = "ff866d71-0c0e-4af0-8437-a4177ed03f2c"]
-struct StadiumAsset(pub Stadium);
 
-#[derive(Default)]
-struct StadiumLoader;
-
-impl AssetLoader for StadiumLoader {
-    fn load<'a>(
-        &'a self,
-        bytes: &'a [u8],
-        load_context: &'a mut LoadContext,
-    ) -> BoxedFuture<'a, Result<(), bevy::asset::Error>> {
-        Box::pin(async move {
-            let data_str = std::str::from_utf8(bytes)?;
-            let stadium_value = parse_to_serde_value(data_str, &ParseOptions::default())?;
-            let stadium_raw: StadiumRaw = serde_json::from_value(stadium_value.unwrap())?;
-            let stadium = stadium_raw.to_stadium();
-            let asset = StadiumAsset(stadium);
-            load_context.set_default_asset(LoadedAsset::new(asset));
-            Ok(())
-        })
-    }
-
-    fn extensions(&self) -> &[&str] {
-        &["json5", "json", "hbs"]
-    }
-}
-
-#[derive(Debug, Resource)]
-struct DataAssets {
-    pub stadium: Handle<StadiumAsset>,
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        transform: Transform {
+            scale: Vec3::new(0.7, -0.7, -1.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,14 +1,20 @@
-use bevy::prelude::*;
+use bevy::{asset::*, prelude::*, reflect::TypeUuid};
 use bevy_egui::{egui, EguiContexts};
+use jsonc_parser::{parse_to_serde_value, ParseOptions};
 
-use crate::AppState;
+use crate::{
+    parser::stadium::{Stadium, StadiumRaw},
+    AppState,
+};
 
 pub struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(setup_menu.in_schedule(OnEnter(AppState::Menu)))
-            .add_system(menu.in_set(OnUpdate(AppState::Menu)))
+        app.add_asset::<StadiumAsset>()
+            .init_asset_loader::<StadiumLoader>()
+            .add_system(setup_menu.in_schedule(OnEnter(AppState::Menu)))
+            .add_systems((menu, load_to_ingame).in_set(OnUpdate(AppState::Menu)))
             .add_system(cleanup_menu.in_schedule(OnExit(AppState::Menu)));
     }
 }
@@ -34,36 +40,98 @@ impl StadiumInfo {
     }
 }
 
+#[derive(Resource, Default)]
+struct AssetsLoading(Vec<HandleUntyped>);
+
+#[derive(Debug, Resource)]
+pub struct DataAssets {
+    pub stadium: Handle<StadiumAsset>,
+}
+
+#[derive(Debug, TypeUuid)]
+#[uuid = "ff866d71-0c0e-4af0-8437-a4177ed03f2c"]
+pub struct StadiumAsset(pub Stadium);
+
+#[derive(Default)]
+pub struct StadiumLoader;
+
+impl AssetLoader for StadiumLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, Result<(), bevy::asset::Error>> {
+        Box::pin(async move {
+            let data_str = std::str::from_utf8(bytes)?;
+            let stadium_value = parse_to_serde_value(data_str, &ParseOptions::default())?;
+            let stadium_raw: StadiumRaw = serde_json::from_value(stadium_value.unwrap())?;
+            let stadium = stadium_raw.to_stadium();
+            let asset = StadiumAsset(stadium);
+            load_context.set_default_asset(LoadedAsset::new(asset));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["json5", "json", "hbs"]
+    }
+}
+
 fn setup_menu(mut commands: Commands) {
-    println!("setup menu");
+    commands.insert_resource(AssetsLoading::default());
     commands.insert_resource(MenuData {
         stadium_info: StadiumInfo::Classic,
     });
 }
 
 fn menu(
-    // mut commands: Commands,
+    mut commands: Commands,
     mut contexts: EguiContexts,
-    // mut next_state: ResMut<NextState<AppState>>,
     mut menu_data: ResMut<MenuData>,
-    // asset_server: Res<AssetServer>,
+    asset_server: Res<AssetServer>,
+    mut loading: ResMut<AssetsLoading>,
 ) {
     egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
-        ui.vertical_centered(|ui| {
-            egui::ComboBox::from_label("Select one!")
-                .selected_text(format!("{:?}", menu_data.stadium_info))
-                .show_ui(ui, |ui| {
-                    ui.selectable_value(
-                        &mut menu_data.stadium_info,
-                        StadiumInfo::Classic,
-                        "Classic",
-                    );
-                    ui.selectable_value(&mut menu_data.stadium_info, StadiumInfo::Big, "Big");
-                });
-        });
+        ui.heading("Stadiums");
+
+        egui::ComboBox::from_id_source("stadium")
+            .selected_text(format!("{:?}", menu_data.stadium_info))
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut menu_data.stadium_info, StadiumInfo::Classic, "Classic");
+                ui.selectable_value(&mut menu_data.stadium_info, StadiumInfo::Big, "Big");
+            });
+
+        if ui.button("Load").clicked() {
+            let stadium = asset_server.load(menu_data.stadium_info.get_path());
+            loading.0.push(stadium.clone_untyped());
+            commands.insert_resource(DataAssets { stadium });
+        }
     });
 }
 
-fn cleanup_menu(mut commands: Commands) {
+fn load_to_ingame(
+    mut commands: Commands,
+    server: Res<AssetServer>,
+    loading: Res<AssetsLoading>,
+    mut next_state: ResMut<NextState<AppState>>,
+) {
+    if loading.0.is_empty() {
+        return;
+    }
+
+    match server.get_group_load_state(loading.0.iter().map(|h| h.id())) {
+        LoadState::Failed => {
+            panic!("Failed to load assets");
+        }
+        LoadState::Loaded => {
+            println!("Assets loaded");
+            commands.remove_resource::<AssetsLoading>();
+            next_state.set(AppState::InGame);
+        }
+        _ => {}
+    }
+}
+
+fn cleanup_menu() {
     println!("cleanup menu")
 }
