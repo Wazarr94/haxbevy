80791aca2eac64c8b1c13cb5950d1572792a285d
Wazarr <jeje_04@live.com>
2023-06-17 10:31:43 +0200

    menu progress

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use bevy::asset::*;
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_prototype_lyon::prelude::*;
+use menu::MenuPlugin;
 use parser::stadium::Stadium;
 use parser::stadium::StadiumRaw;
-use renderer::RendererPlugin;
 
 use jsonc_parser::{parse_to_serde_value, ParseOptions};
 
@@ -13,6 +12,29 @@ mod menu;
 mod parser;
 mod renderer;
 
+fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
+        .add_plugin(ShapePlugin)
+        .add_plugin(MenuPlugin)
+        // .add_plugin(WorldInspectorPlugin::new())
+        // .add_plugin(RendererPlugin)
+        .add_asset::<StadiumAsset>()
+        .init_asset_loader::<StadiumLoader>()
+        .run();
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum AppState {
+    #[default]
+    Menu,
+    InGame,
+}
 #[derive(Debug, TypeUuid)]
 #[uuid = "ff866d71-0c0e-4af0-8437-a4177ed03f2c"]
 struct StadiumAsset(pub Stadium);
@@ -45,34 +67,4 @@ impl AssetLoader for StadiumLoader {
 #[derive(Debug, Resource)]
 struct DataAssets {
     pub stadium: Handle<StadiumAsset>,
-}
-
-fn main() {
-    // When building for WASM, print panics to the browser console
-    #[cfg(target_arch = "wasm32")]
-    console_error_panic_hook::set_once();
-
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(ShapePlugin)
-        .add_plugin(WorldInspectorPlugin::new())
-        .add_plugin(RendererPlugin)
-        .add_asset::<StadiumAsset>()
-        .init_asset_loader::<StadiumLoader>()
-        .add_startup_system(setup)
-        .run();
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let map_path = "stadiums/obstacle-map-winky.json5";
-    let stadium: Handle<StadiumAsset> = asset_server.load(map_path);
-    commands.insert_resource(DataAssets { stadium });
-
-    commands.spawn(Camera2dBundle {
-        transform: Transform {
-            scale: Vec3::new(0.7, -0.7, -1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,1 +1,69 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts};
 
+use crate::AppState;
+
+pub struct MenuPlugin;
+
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(setup_menu.in_schedule(OnEnter(AppState::Menu)))
+            .add_system(menu.in_set(OnUpdate(AppState::Menu)))
+            .add_system(cleanup_menu.in_schedule(OnExit(AppState::Menu)));
+    }
+}
+
+#[derive(Resource, Default)]
+struct MenuData {
+    stadium_info: StadiumInfo,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+enum StadiumInfo {
+    #[default]
+    Classic,
+    Big,
+}
+
+impl StadiumInfo {
+    fn get_path(&self) -> &str {
+        match self {
+            StadiumInfo::Classic => "stadiums/classic.json5",
+            StadiumInfo::Big => "stadiums/big.json5",
+        }
+    }
+}
+
+fn setup_menu(mut commands: Commands) {
+    println!("setup menu");
+    commands.insert_resource(MenuData {
+        stadium_info: StadiumInfo::Classic,
+    });
+}
+
+fn menu(
+    // mut commands: Commands,
+    mut contexts: EguiContexts,
+    // mut next_state: ResMut<NextState<AppState>>,
+    mut menu_data: ResMut<MenuData>,
+    // asset_server: Res<AssetServer>,
+) {
+    egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
+        ui.vertical_centered(|ui| {
+            egui::ComboBox::from_label("Select one!")
+                .selected_text(format!("{:?}", menu_data.stadium_info))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(
+                        &mut menu_data.stadium_info,
+                        StadiumInfo::Classic,
+                        "Classic",
+                    );
+                    ui.selectable_value(&mut menu_data.stadium_info, StadiumInfo::Big, "Big");
+                });
+        });
+    });
+}
+
+fn cleanup_menu(mut commands: Commands) {
+    println!("cleanup menu")
+}
