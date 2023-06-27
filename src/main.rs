c8c8a64d9cf976047e25d4b4f04daecffc0196f0
Wazarr <jeje_04@live.com>
2023-06-27 19:48:49 +0200

    add fit_canvas_to_parent back

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 present_mode: PresentMode::AutoVsync,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
                 // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
                 prevent_default_event_handling: false,
                 ..default()
